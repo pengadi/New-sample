c62857abfd6e3d9a3001630407f1a57fa762b62f
saidurga-30 <sai.timmisetty@gmail.com>
2023-08-24 08:31:50 +0530

    Add project files.

---
+++ New sample/New sample.cpp
@@ -0,0 +1,13 @@
+// New sample.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void fun()
+{
+    cout << "fun() called" << endl;
+}
+int main()
+{
+    fun();
+}
